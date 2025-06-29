81bb85cd3176e1fc336b36f391fc99ed9d441998
SARTHAKSHARMW <125258551+SARTHAKSHARMW@users.noreply.github.com>
2025-06-29 20:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.47%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        // ok so in water bottle problem questions there are two numbottles and numexchange 3 empty bottles get 1 full water bottle 
+
+
+
+        // lets take the help of the second bottle  example there are 15 filled bottles and num of bottles that can be exchanged is 4 so if we divide 15 by 4 then we will get unfilled or remaining bottles and the new bottles and then simply add the remaining bottles and the new bottles again and again
+        // so there are num of bottles bottles exchange remaining bottles new bottles 
+        int ans = numBottles;
+        while(numBottles >= numExchange){
+            int newBottles = numBottles / numExchange ;
+
+            int remainBottles = numBottles % numExchange ; // as we have to find remainder ; 
+            
+            ans =  ans + newBottles ;
+
+            numBottles = newBottles + remainBottles ;
+        }
+        return ans ;
+    }
+}
