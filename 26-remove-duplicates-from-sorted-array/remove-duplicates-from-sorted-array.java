79e63e2ad724297846a48a4584c7db5ae68eab01
SARTHAKSHARMW <125258551+SARTHAKSHARMW@users.noreply.github.com>
2025-08-11 19:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (79.83%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -14,4 +14,10 @@ class Solution {
         return i + 1; // length of unique elements
     }
 }
-// so the main logic behind this code is that 
+// so the main logic behind this code is that i=0 (0), j=1 (0) → duplicate → skip
+
+//i=0 (0), j=2 (1) → new → i=1, nums[1]=1
+
+//i=1 (1), j=3 (1) → duplicate → skip
+
+//i=1 (1), j=5 (2) → new → i=2, nums[2]=2
